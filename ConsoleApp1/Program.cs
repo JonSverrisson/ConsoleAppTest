2d773aeff5bcf4613a2bcf2d9a171e32c77c9bba
Jon_Sverrisson <jon.sverrisson@xlent.se>
2018-03-02 13:04:57 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = "C:/Users/Jon_Sverrisson/Source/Repos/ConsoleApp1/ConsoleApp1/data.json";
+            using (StreamReader str = new StreamReader(filepath))
+            {
+                string json = str.ReadToEnd();
+                var items = JsonConvert.DeserializeObject<Model>(json);
+
+                Console.WriteLine(items);
+            }
+
+        }
+    }
+}
